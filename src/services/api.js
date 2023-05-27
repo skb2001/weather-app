20a79a226be3562a3bb23d91958deac1c1e3c74f
skb2001 <88270396+skb2001@users.noreply.github.com>
2023-05-27 16:14:24 +0530

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_KEY = '443aaf46506f595dd739a12c4077a1f7';
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+export const getWeather = async (city, country) => {
+    try {
+        let response = await axios.get(`${API_URL}?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        return response.data;
+    } catch (error) {
+        console.log('Error while calling the api ', error.message);
+        return error.response;
+    }
+}
